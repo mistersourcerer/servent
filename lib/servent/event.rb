33f6bc4f703c7a93df447ef9a4eaacefd030c334
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2017-11-10 22:56:02 +0100

    Make `Event` responsible only for type/data parse.

--- lib/servent/event.rb
+++ lib/servent/event.rb
@@ -13,17 +13,26 @@ module Servent
 
     def parse_line(line)
       return unless line.include?(Servent::COLON)
+      normalize_type_and_data(* line.split(":"))
+    end
 
-      @type, data = line.split(":")
-      @data << "\n" unless @data.empty?
-      @data << remove_extra_space(data)
-      #if type != @type; # new event?
+    def normalize_type_and_data(type, data)
+      if type == "event"
+        @type = remove_extra_space data.chomp
+      else
+        @type = "data" if @type.nil?
+        concat_data data
+      end
     end
 
     def remove_extra_space(raw_data)
       return raw_data unless raw_data[0] == "\u{0020}"
-
       raw_data[1..(raw_data.length - 1)]
+    end
+
+    def concat_data(data)
+      @data << "\n" unless @data.empty?
+      @data << remove_extra_space(data)
     end
   end
 end
--- spec/servent/event_spec.rb
+++ spec/servent/event_spec.rb
@@ -1,2 +1,23 @@
 RSpec.describe Servent::Event do
+  context "data:" do
+    it "recognizes a simple `data: omg` stream" do
+      event = described_class.new "data: omg"
+
+      expect(event).to be_event("data", "omg")
+    end
+
+    it 'removes just one space from message `data:\s\somg`' do
+      event = described_class.new "data:\s\somg"
+
+      expect(event).to be_event("data", " omg")
+    end
+  end
+
+  context "custom event types" do
+    it 'recognizes a type with its content `event: omg\ndata: lol`' do
+      event = described_class.new "event: omg\ndata: lol"
+
+      expect(event).to be_event("omg", "lol")
+    end
+  end
 end
