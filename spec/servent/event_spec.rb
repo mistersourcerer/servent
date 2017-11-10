ea6572c15d1dea2343a95d7134a107cf9db0ca4d
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2017-11-10 13:37:26 +0100

    Make the custom message matcher more reusable.

--- spec/servent/event_spec.rb
+++ spec/servent/event_spec.rb
@@ -6,41 +6,41 @@ RSpec.describe Servent::Event do
         it 'recognizes simple `data: omg\n\n` pattern as a complete event' do
           event.parse "data:omg\n\r\n"
 
-          expect(event).to be_data "omg"
+          expect(event).to be_message "data", "omg"
         end
 
         it "remove the first space from `data`" do
           event.parse "data: omg\n\r\n"
 
-          expect(event).to be_data "omg"
+          expect(event).to be_message "data", "omg"
         end
 
         it 'recognizes when return comes later: `data: omg\n\n\r`' do
           event.parse "data: omg\n\n\r"
 
-          expect(event).to be_data "omg"
+          expect(event).to be_message "data", "omg"
         end
 
         it 'recognizes when first line is delimited by \r `data: omg\r\n\r`' do
           event.parse "data: omg\n\n\r"
 
-          expect(event).to be_data "omg"
+          expect(event).to be_message "data", "omg"
         end
       end
     end
   end
 end
 
-RSpec::Matchers.define :be_data do |data|
+RSpec::Matchers.define :be_message do |type, data|
   match do |event|
-    event.type == "data" && event.data == data
+    event.type == type && event.data == data
   end
 
   failure_message do |event|
     message = ""
 
-    if event.type != "data"
-      message << "expected event#type to be 'data' but was #{event.type}"
+    if event.type != type
+      message << "expected event#type to be '#{type}' but was #{event.type}"
     end
 
     if event.data != data
