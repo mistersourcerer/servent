225f37c09a4e055eed1d5ea24d697fd0f7cc7182
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2017-11-10 22:55:25 +0100

    Move parsing outisde the `Event` itself.

---
+++ spec/servent/stream_spec.rb
@@ -0,0 +1,32 @@
+RSpec.describe Servent::Stream do
+  describe "#parse" do
+    context ":comment" do
+      it "ignores streams starting with `:`" do
+        stream = described_class.new ":omg a comment!"
+
+        events = stream.parse
+        expect(events.count).to eq 0
+      end
+    end
+
+    context "data:" do
+      it "recognizes a simple `data: omg` stream" do
+        stream = described_class.new "data: omg"
+        events = stream.parse
+
+        expect(events.count).to eq 1
+      end
+    end
+
+    context "multiline stream" do
+      context "custom named events" do
+        it 'recognizes a type with its content `event: omg\ndata: lol`' do
+          stream = described_class.new "event: omg\ndata: lol"
+          events = stream.parse
+
+          expect(events.count).to eq 1
+        end
+      end
+    end
+  end
+end
