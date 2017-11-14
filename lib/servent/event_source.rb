2ad98123b079a0153c50c5704b159338fcc9579b
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2017-11-14 21:22:13 +0100

    Make `Faraday` configurable from client-side.

--- lib/servent/event_source.rb
+++ lib/servent/event_source.rb
@@ -3,16 +3,24 @@ require "servent/event"
 
 module Servent
   class EventSource
-    def initialize(url, adapter: nil)
-      # if adapter.nil?
-      # call faraday and allow client to configure it (builder)
+    def initialize(url, adapter: nil, &configurator)
       @uri     = URI(url)
+      adapter  = create_faraday(&configurator) if adapter.nil?
       @adapter = adapter
     end
 
     def start
       @adapter.get(@uri.path) do |req|
         req.headers["Accept"] = "text/event-stream"
+        yield(req) if block_given?
+      end
+    end
+
+    private
+
+    def create_faraday
+      Faraday.new(url: @uri) do |faraday|
+        yield(faraday) if block_given?
       end
     end
   end
--- spec/servent/event_source_spec.rb
+++ spec/servent/event_source_spec.rb
@@ -9,15 +9,32 @@ RSpec.describe Servent::EventSource do
 
   subject(:event_source) { described_class.new url, adapter: adapter }
 
+  describe ".new" do
+    it "yields the `Faraday` object for further (client) connection" do
+      expect { |configuration_block|
+        described_class.new(url, &configuration_block)
+      }.to yield_with_args(Faraday::Connection)
+    end
+  end
+
   describe "#start" do
-    it "sends a GET request with right headers" do
+    before do
       stubs.get("/event-stream") do |env|
         expect(env.request_headers["Accept"]).to eq "text/event-stream"
         [200, {}, ""]
       end
+    end
+
+    it "sends a GET request with right headers" do
       event_source.start
 
       stubs.verify_stubbed_calls
+    end
+
+    it "yields the (faraday) request object to a block if it is passed" do
+      expect { |configuration_block|
+        event_source.start(&configuration_block)
+      }.to yield_with_args(Faraday::Request)
     end
 
     context "reconnection"
