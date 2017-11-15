6c8f0d7b25b96f8dbdadcbe01a13df6e697591a1
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2017-11-15 10:29:23 +0100

    :nail_care: Cleanup spec configuration.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,12 +6,8 @@ spec_dir = File.expand_path("../", __FILE__)
 Dir["#{spec_dir}/support/**/*.rb"].each { |f| require f }
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
-
   config.expect_with :rspec do |c|
     c.syntax = :expect
   end
